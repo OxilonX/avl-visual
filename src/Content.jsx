2ae57522af7d01e8482ccac75532911f04a2c72a
Boulmehad Abderrahmane <boabmeuni01@gmail.com>
2025-11-21 13:06:20 +0100

    add project structure

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,8 @@
-import Tree from "./Tree";
+import { useState } from "react";
 import { Button, Stack, Input } from "@mui/material";
 export default function Content() {
+  const [insertVal, setInsertVal] = useState("");
+  const [deleteVal, setDeleteVal] = useState("");
   return (
     <>
       <header style={{ padding: "16px 24px" }}>
@@ -13,12 +15,15 @@ export default function Content() {
               variant="filled"
               color="success"
               placeholder="Insert node value..."
+              value={insertVal}
+              onChange={(e) => setInsertVal(e.target.value)}
             />
             <Button
               color="success"
               variant="contained"
               size="small"
               sx={{ padding: "10px 20px" }}
+              onClick={() => setInsertVal("")}
             >
               insert
             </Button>
@@ -31,28 +36,38 @@ export default function Content() {
               variant="filled"
               color="error"
               placeholder="Delete node value..."
+              value={deleteVal}
+              onChange={(e) => setDeleteVal(e.target.value)}
             />
             <Button
               color="error"
               variant="contained"
               size="small"
               sx={{ padding: "10px 20px" }}
+              onClick={() => setDeleteVal("")}
             >
               delete
             </Button>
           </Stack>
-          <Button
-            color="primary"
-            variant="contained"
-            sx={{ padding: "10px 20px", marginX: "auto" }}
-          >
-            Clear
-          </Button>
+          <Stack direction={"row"} gap={2} marginX={"auto"}>
+            <Button
+              color="primary"
+              variant="contained"
+              sx={{ padding: "10px 20px" }}
+            >
+              Clear
+            </Button>
+            <Button
+              color="secondary"
+              variant="contained"
+              sx={{ padding: "10px 20px" }}
+            >
+              reset
+            </Button>
+          </Stack>
         </Stack>
       </header>
-      <div id="tree">
-        <Tree />
-      </div>
+      <div id="tree"></div>
     </>
   );
 }
--- src/Tree.jsx
+++
@@ -1,7 +0,0 @@
-export default function Tree() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
